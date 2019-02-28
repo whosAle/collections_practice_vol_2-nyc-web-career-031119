de435c3d542b05633364176b07ae5da4e1e83c69
A Belgrave <abelgrave95@gmail.com>
2019-02-28 18:54:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,7 +20,7 @@ def count_elements(array)
     puts hash
     hash.each do |key, name|
       if count[key][name]
-        count[name][:count] += 1
+        count[key][name][:count] += 1
       else
         count[key] = name
         count[name][:count] = 1
