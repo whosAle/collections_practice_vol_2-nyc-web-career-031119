a73459f73e9eb40066a32c4804b1038e8efffaf8
A Belgrave <abelgrave95@gmail.com>
2019-02-28 19:18:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,6 +39,6 @@ end
 def find_cool(array)
   result = []
 
-  array.collect { |hash| hash if hash[:temperature] == "cool"  }
+  array.collect { |hash| hash.select { |k, v| v = "cool"  }}# if hash[:temperature] == "cool"  }
 
 end
